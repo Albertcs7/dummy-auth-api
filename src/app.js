f14f9ef6396f4b6b77049d56a8147f48559008d5
Albertcs7 <albertacs2004@gmail.com>
2026-04-23 16:07:47 +0530

    afedwef

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
+const users = require("./data/users.data");
 
 const app = express();
+
+const sanitizeUser = ({ password, ...user }) => user;
 
 app.use(cors());
 app.use(express.json());
@@ -11,7 +14,19 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Dummy Auth API Running");
+  res.json({
+    UID: "3570",
+    password: "Aditi01*",
+    username: "Aditi Nair",
+    role: "ADMIN",
+    modules: [
+      {
+        module: "insurance",
+        access: true,
+        permissions: { read: true, write: true, delete: false },
+      },
+    ],
+  },);
 });
 
 module.exports = app;
